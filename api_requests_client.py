3c374bf0389b909c9c3e9a3331207db94917c16c
jamesr <jamesruss3@gmail.com>
2025-09-04 22:59:24 +0100

    Separated out api requests, tidy up and some logging adjustments
    Signed-off-by: jamesr <jamesruss3@gmail.com>

---
+++ api_requests_client.py
@@ -0,0 +1,104 @@
+
+
+import logging
+import random
+import time
+import requests
+
+
+def fetch_social_handle(email, max_retries=5):
+    """Fetch API data for a given email with retries and error handling."""
+    if not email:
+        return {"data": None, "status": "No email provided"}
+
+    url = "http://localhost:5000/enrichment"
+    headers = {
+        "X-API-KEY": "SECRET_KEY_123"
+    }
+    params = {
+        "email": email
+    }
+    params = {"email": email}
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            # Raise for status for non-2xx responses
+            response.raise_for_status()
+            # Check is no data returned from endpoint
+            if response.status_code == 204 or not response.json():
+                return {"data": None, "status": f"No profile found for {email}"}
+
+            return {"data": response.json(), "status": "Successfully found social handle for user"}
+
+        except requests.HTTPError as http_err:
+            status = response.status_code
+            if status == 429:  # Rate limit
+                wait_time = 2 ** retries + random.random()  # exponential backoff + jitter
+                logging.warning(f"Rate limited for {email}. Waiting {wait_time:.1f} seconds before retrying...")
+                time.sleep(wait_time)
+                retries += 1
+            elif 500 <= status < 600:  # Server errors
+                wait_time = 2 ** retries + random.random()
+                logging.warning(f"Server error ({status}) for {email}. Retrying in {wait_time:.1f} seconds...")
+                time.sleep(wait_time)
+                retries += 1
+            elif status == 404:  # Missing profile
+                return {"data": None, "status": "Profile not found (404)"}
+            else:
+                logging.error(f"HTTP error {status}: {http_err}")
+                return {"data": None, "status": f"HTTP error {status}: {http_err}"}
+
+        except requests.RequestException as e:
+            # Handle network errors
+            wait_time = 2 ** retries + random.random()
+            logging.warning(f"Network error for {email}: {e}. Retrying in {wait_time:.1f} seconds...")
+            time.sleep(wait_time)
+            retries += 1
+    logging.error(f"Error fetching social handle for customer. Max retries ({max_retries}) exceeded")
+    return {"data": None, "status": f"Max retries ({max_retries}) exceeded"}
+
+def send_enriched_data(data, max_retries=5):
+    # Send encriched customer data to submissions API with retries and error handling.
+    url = "http://localhost:5000/submission"
+    headers = {
+        "X-API-KEY": "SECRET_KEY_123",
+        "Content-Type": "application/json"
+    }
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            response = requests.post(url, headers=headers, json=data)
+            # Raise for status for non-2xx responses
+            response.raise_for_status()
+            return response.json()
+
+        except requests.HTTPError as http_err:
+            status = response.status_code
+            if status == 429:  # Rate limit
+                wait_time = 2 ** retries + random.random()  # exponential backoff + jitter
+                logging.warning(f"Rate limited waiting {wait_time:.1f} seconds before retrying...")
+                time.sleep(wait_time)
+                retries += 1
+            elif 500 <= status < 600:  # Server errors
+                wait_time = 2 ** retries + random.random()
+                logging.warning(f"Server error ({status}) while sending CustomerID: {data['customer_id']}. Retrying in {wait_time:.1f} seconds...")
+                time.sleep(wait_time)
+                retries += 1
+            elif status == 422:  # Validation Error
+                logging.error(f"Data validation error: {response.json()}")
+                return response.json()
+            else:
+                logging.error(f"HTTP error {status}: {http_err}")
+                return f"HTTP error {status}: {http_err}"
+
+        except requests.RequestException as e:
+            # Handle network errors
+            wait_time = 2 ** retries + random.random()
+            logging.warning("Network error while sending CustomerID: {data['customer_id']} with Error: {e}. Retrying in {wait_time:.1f} seconds...")
+            time.sleep(wait_time)
+            retries += 1
+    logging.error(f"Max retries ({max_retries}) exceeded while sending CustomerID: {data['customer_id']}")
+    return f"Max retries ({max_retries}) exceeded while sending CustomerID: {data['customer_id']}"
--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import time
-import random
 import sqlite3
 import pandas as pd
-import requests
 import logging
+from api_requests_client import fetch_social_handle, send_enriched_data
 
 # Logging config
 def setup_logging(log_file="customer_order_enrich.log"):
     logging.basicConfig(
         filename=log_file,
-        filemode='w',          # overwrite each run
+        filemode='w',  
         level=logging.INFO,
         format='%(asctime)s | %(levelname)s | %(message)s'
     )
-    # Also write log to console
+    # Write log to console
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
     console.setFormatter(formatter)
     logging.getLogger().addHandler(console)
-
 
 def parse_date(date_str):
     for fmt in ("%d/%m/%Y", "%Y-%m-%d"):
@@ -28,7 +25,6 @@ def parse_date(date_str):
         except:
             continue
     return pd.NaT  # Not a valid date
-
 
 def get_customer_orders():
     conn = sqlite3.connect("database/client_data.db")
@@ -41,106 +37,11 @@ def get_customer_orders():
     conn.close()
     return df
 
-
-def fetch_enriched_customer_data(email, max_retries=5):
-    """Fetch API data for a given email with retries and error handling."""
-    if not email:
-        return {"data": None, "status": "No email provided"}
-
-    url = "http://localhost:5000/enrichment"
-    headers = {
-        "X-API-KEY": "SECRET_KEY_123"
-    }
-    params = {
-        "email": email
-    }
-    params = {"email": email}
-    retries = 0
-
-    while retries < max_retries:
-        try:
-            response = requests.get(url, headers=headers, params=params)
-            # Raise for status for non-2xx responses
-            response.raise_for_status()
-            # Check is no data returned from enriched data endpoint
-            if response.status_code == 204 or not response.json():  # 204 = No Content
-                return {"data": None, "status": f"No profile found for {email}"}
-
-            return {"data": response.json(), "status": "Successfully found social handle for user"}
-
-        except requests.HTTPError as http_err:
-            status = response.status_code
-            if status == 429:  # Rate limit
-                wait_time = 2 ** retries + random.random()  # exponential backoff + jitter
-                logging.info(f"Rate limited for {email}. Waiting {wait_time:.1f} seconds before retrying...")
-                time.sleep(wait_time)
-                retries += 1
-            elif 500 <= status < 600:  # Server errors
-                wait_time = 2 ** retries + random.random()
-                logging.info(f"Server error ({status}) for {email}. Retrying in {wait_time:.1f} seconds...")
-                time.sleep(wait_time)
-                retries += 1
-            elif status == 404:  # Missing profile
-                return {"data": None, "status": "Profile not found (404)"}
-            else:
-                return {"data": None, "status": f"HTTP error {status}: {http_err}"}
-
-        except requests.RequestException as e:
-            # Handle network errors
-            wait_time = 2 ** retries + random.random()
-            logging.info(f"Network error for {email}: {e}. Retrying in {wait_time:.1f} seconds...")
-            time.sleep(wait_time)
-            retries += 1
-
-    return {"data": None, "status": f"Max retries ({max_retries}) exceeded"}
-
-def send_enriched_customer_data(data, max_retries=5):
-    # Send encriched customer data to submissions API with retries and error handling.
-    url = "http://localhost:5000/submission"
-    headers = {
-        "X-API-KEY": "SECRET_KEY_123",
-        "Content-Type": "application/json"
-    }
-    retries = 0
-
-    while retries < max_retries:
-        try:
-            response = requests.post(url, headers=headers, json=data)
-            # Raise for status for non-2xx responses
-            response.raise_for_status()
-            # Check is no data returned from enriched data endpoint
-            return {"data": response.json(), "status": "Successfully posted user data"}
-
-        except requests.HTTPError as http_err:
-            status = response.status_code
-            if status == 429:  # Rate limit
-                wait_time = 2 ** retries + random.random()  # exponential backoff + jitter
-                logging.info(f"Rate limited waiting {wait_time:.1f} seconds before retrying...")
-                time.sleep(wait_time)
-                retries += 1
-            elif 500 <= status < 600:  # Server errors
-                wait_time = 2 ** retries + random.random()
-                logging.info(f"Server error ({status}) while sending CustomerID: {data['customer_id']}. Retrying in {wait_time:.1f} seconds...")
-                time.sleep(wait_time)
-                retries += 1
-            elif status == 422:  # Validation Error
-                return {"data": response.json(), "status": "Validation Error (422)"}
-            else:
-                return {"data": None, "status": f"HTTP error {status}: {http_err}"}
-
-        except requests.RequestException as e:
-            # Handle network errors
-            wait_time = 2 ** retries + random.random()
-            logging.info("Network error while sending CustomerID: {data['customer_id']} with Error: {e}. Retrying in {wait_time:.1f} seconds...")
-            time.sleep(wait_time)
-            retries += 1
-
-    return {"data": None, "status": f"Max retries ({max_retries}) exceeded while sending CustomerID: {data['customer_id']}"}
-
 def filter_by_city(city, df):
-    # standardise city formats in the dataset and filter based on selected city
+    # Standardise city formats in the dataset
     df['city'] = df['city'].str.strip().str.lower()
-    df = df[df['city'] == city]
+    # Filter based on selected city making sure city input is also converted to lowercase
+    df = df[df['city'] == city.strip().lower()]
     return df
 
 def filter_by_start_date(start_date, df):
@@ -150,7 +51,7 @@ def filter_by_start_date(start_date, df):
     df = df[(df['order_date'] >= start_date)]
     return df
 
-if __name__ == "__main__":
+def enrich_customer_orders():
     setup_logging()
     start_date = '2024-09-01'
     city = 'manchester'
@@ -176,7 +77,7 @@ if __name__ == "__main__":
     for idx, row in agg_df.iterrows():
         logging.info(f"Fetching social handle for row {idx} | email: {row['email']}")
         # attempt to fetch social handle for each customer
-        result = fetch_enriched_customer_data(row['email'])
+        result = fetch_social_handle(row['email'])
         # Set each column separately
         social_handle = None
         if result['data'] is not None:
@@ -195,14 +96,19 @@ if __name__ == "__main__":
             "social_handle": social_handle or ""
         }
         logging.info(f"Sending data to submissions endpoint")
-        result = send_enriched_customer_data(post_data)
+        result = send_enriched_data(post_data)
         logging.info(f"Post Status: {result}")
 
     # Output final dataframe as CSV
     logging.info(f"Writing output to CSV")
     agg_df.to_csv(f"customer_orders_since_{start_date}.csv", index=False)
+    logging.info(f"Process complete.")
 
-    logging.info(f"Process complete")
+
+if __name__ == "__main__":
+    enrich_customer_orders()
+
+  
 
 
 
